94c1111fe1fbf7f1e508f6fa89bfae1666c3b24a
BenjaminFalsetto <benjaminfalsetto@ocsbstudent.ca>
2018-10-11 15:27:02 -0400

    Set the max/min for the nud. Took and added screenshots

--- GuessGameBenF/GuessGameBenF/GuessGameForm.Designer.cs
+++ GuessGameBenF/GuessGameBenF/GuessGameForm.Designer.cs
@@ -39,45 +39,65 @@
             // lblGuessNum
             // 
             this.lblGuessNum.AutoSize = true;
-            this.lblGuessNum.Font = new System.Drawing.Font("Onky", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGuessNum.Location = new System.Drawing.Point(12, 94);
+            this.lblGuessNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessNum.Location = new System.Drawing.Point(16, 116);
+            this.lblGuessNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGuessNum.Name = "lblGuessNum";
-            this.lblGuessNum.Size = new System.Drawing.Size(557, 27);
+            this.lblGuessNum.Size = new System.Drawing.Size(465, 31);
             this.lblGuessNum.TabIndex = 0;
             this.lblGuessNum.Text = "Guess the number between 1 and 10!";
             // 
             // nudGuess
             // 
-            this.nudGuess.Location = new System.Drawing.Point(578, 102);
+            this.nudGuess.Location = new System.Drawing.Point(771, 126);
+            this.nudGuess.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nudGuess.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudGuess.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.nudGuess.Name = "nudGuess";
-            this.nudGuess.Size = new System.Drawing.Size(120, 20);
+            this.nudGuess.Size = new System.Drawing.Size(160, 22);
             this.nudGuess.TabIndex = 1;
+            this.nudGuess.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // lblCorrect
             // 
             this.lblCorrect.AutoSize = true;
-            this.lblCorrect.Font = new System.Drawing.Font("Onky", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCorrect.Location = new System.Drawing.Point(12, 229);
+            this.lblCorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorrect.Location = new System.Drawing.Point(16, 282);
+            this.lblCorrect.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCorrect.Name = "lblCorrect";
-            this.lblCorrect.Size = new System.Drawing.Size(286, 27);
+            this.lblCorrect.Size = new System.Drawing.Size(236, 31);
             this.lblCorrect.TabIndex = 3;
             this.lblCorrect.Text = "Correct! Good job!";
             // 
             // lblIncorrect
             // 
             this.lblIncorrect.AutoSize = true;
-            this.lblIncorrect.Font = new System.Drawing.Font("Onky", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIncorrect.Location = new System.Drawing.Point(12, 291);
+            this.lblIncorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncorrect.Location = new System.Drawing.Point(16, 358);
+            this.lblIncorrect.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIncorrect.Name = "lblIncorrect";
-            this.lblIncorrect.Size = new System.Drawing.Size(310, 27);
+            this.lblIncorrect.Size = new System.Drawing.Size(248, 31);
             this.lblIncorrect.TabIndex = 4;
             this.lblIncorrect.Text = "Incorrect, try again!";
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(330, 166);
+            this.btnCheck.Location = new System.Drawing.Point(440, 204);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.Size = new System.Drawing.Size(100, 28);
             this.btnCheck.TabIndex = 5;
             this.btnCheck.Text = "Check";
             this.btnCheck.UseVisualStyleBackColor = true;
@@ -85,14 +105,15 @@
             // 
             // GuessGameForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 383);
+            this.ClientSize = new System.Drawing.Size(969, 471);
             this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.lblIncorrect);
             this.Controls.Add(this.lblCorrect);
             this.Controls.Add(this.nudGuess);
             this.Controls.Add(this.lblGuessNum);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GuessGameForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.nudGuess)).EndInit();
