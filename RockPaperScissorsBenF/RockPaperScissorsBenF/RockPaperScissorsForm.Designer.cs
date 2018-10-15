8143a0ea723d574a3dc69853a83c0d4f3b1da90b
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-15 10:50:16 -0400

    Worked on Rock Paper Scissors

--- RockPaperScissorsBenF/RockPaperScissorsBenF/RockPaperScissorsForm.Designer.cs
+++ RockPaperScissorsBenF/RockPaperScissorsBenF/RockPaperScissorsForm.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(41, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // RockPaperScissorsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(506, 392);
+            this.Controls.Add(this.groupBox1);
             this.Name = "RockPaperScissorsForm";
-            this.Text = "Rock, Paper";
+            this.Text = "Rock, Paper, Scissors";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
