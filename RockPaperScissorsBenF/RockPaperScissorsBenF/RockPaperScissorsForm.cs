832173c898f68e5fc93841730371ab40d72e7fae
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-15 11:05:36 -0400

    Worked on Rock Paper Scissors

--- RockPaperScissorsBenF/RockPaperScissorsBenF/RockPaperScissorsForm.cs
+++ RockPaperScissorsBenF/RockPaperScissorsBenF/RockPaperScissorsForm.cs
@@ -12,9 +12,25 @@ namespace RockPaperScissorsBenF
 {
     public partial class RockPaperScissorsForm : Form
     {
+        //declare global variables and constants
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 3;
+        Random randomNumberGenerator;
         public RockPaperScissorsForm()
         {
             InitializeComponent();
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //declare constants
+            const int ROCK = 1;
+            const int PAPER = 2;
+            const int SCISSORS = 3;
+
+            //get user guess
+
         }
     }
 }
