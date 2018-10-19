791074d4d2a09b156f6b40a2a39e72c806a0b9d9
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-19 18:10:00 -0400

    Completed Day 21

---
+++ ProgressBarBenF/ProgressBarBenF/ProgressBarForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ProgressBarBenF
+{
+    partial class ProgressBarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.prbProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(260, 63);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 37);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // prbProgress
+            // 
+            this.prbProgress.Location = new System.Drawing.Point(56, 149);
+            this.prbProgress.Name = "prbProgress";
+            this.prbProgress.Size = new System.Drawing.Size(498, 23);
+            this.prbProgress.TabIndex = 1;
+            // 
+            // ProgressBarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 334);
+            this.Controls.Add(this.prbProgress);
+            this.Controls.Add(this.btnStart);
+            this.Name = "ProgressBarForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar prbProgress;
+    }
+}
+
---
+++ ProgressBarBenF/ProgressBarBenF/ProgressBarForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBarBenF
+{
+    public partial class ProgressBarForm : Form
+    {
+        public ProgressBarForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            const int MAX_PROGRESS_BAR_VALUE = 100;
+            int progBarVal = 0;
+
+            //increment the progress bar by 1 each time until
+            //it has reached its max value
+            do
+            {
+                this.prbProgress.Value = progBarVal;
+                progBarVal = progBarVal + 1;
+                this.Refresh();
+            } while (progBarVal <= MAX_PROGRESS_BAR_VALUE);
+        }
+    }
+}
