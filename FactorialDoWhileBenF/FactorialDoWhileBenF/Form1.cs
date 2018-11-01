ed37be76046260e9956fc9e0e0bb7ffdd40d7383
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-10-31 21:01:40 -0400

    Update FactorialDoWhile

---
+++ FactorialDoWhileBenF/FactorialDoWhileBenF/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialDoWhileBenF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double factorialAnswer, factorialNumber;
+            int factorialCounter;
+
+            //clear the items from the listbox
+            this.lstFactorialNumbers.Items.Clear();
+
+            //initialize the final answer to 1
+            factorialAnswer = 1;
+
+            //get the number from the user
+            factorialCounter = 0;
+
+            //multiply the counter by the next incremented number until it reaches the user's number
+            do
+            {
+                //increment the counter by 1
+                factorialCounter++;
+
+                //list the counter number in the listbox for the user to see
+                lstFactorialNumbers.Items.Add("" + factorialCounter);
+            } while ();
+        }
+    }
+}
