e7ae7f31c543b324125f08fc4e4d3dd64bc16228
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-04 10:18:24 -0500

    Update WalkingMan

---
+++ WalkingManBenF/WalkingManBenF/WalkingManForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WalkingManBenF
+{
+    partial class WalkingManForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picManInBox = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picManInBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picManInBox
+            // 
+            this.picManInBox.Location = new System.Drawing.Point(42, 138);
+            this.picManInBox.Name = "picManInBox";
+            this.picManInBox.Size = new System.Drawing.Size(155, 140);
+            this.picManInBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picManInBox.TabIndex = 0;
+            this.picManInBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(82, 93);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // WalkingManForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 300);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picManInBox);
+            this.Name = "WalkingManForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picManInBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picManInBox;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ WalkingManBenF/WalkingManBenF/WalkingManForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WalkingManBenF
+{
+    public partial class WalkingManForm : Form
+    {
+        public WalkingManForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //declare local variables and constants
+            const byte MAX_FRAMES = 10;
+            int manInBoxCounter = 1;
+            
+            //continue the loop while the frame counter has not reached the max amount of frames
+            while (manInBoxCounter < MAX_FRAMES + 1)
+            {
+                if (manInBoxCounter == 1)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk1;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 2)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk2;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 3)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk3;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if(manInBoxCounter == 4)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk4;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 5)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk5;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 6)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk6;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 7)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk7;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 8)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk8;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 9)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk9;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+                else if (manInBoxCounter == 10)
+                {
+                    this.picManInBox.Image = Properties.Resources.walk10;
+                    this.Refresh();
+                    manInBoxCounter++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
