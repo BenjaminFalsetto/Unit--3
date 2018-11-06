045517f1201a774b1c8272855af1de14068a9fc6
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-06 11:06:00 -0500

    ASgd

---
+++ RunningAverageBenF/RunningAverageBenF/RunningAverageForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RunningAverageBenF
+{
+    partial class frmRunningAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddToAvg = new System.Windows.Forms.Button();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.lblAddToAvg = new System.Windows.Forms.Label();
+            this.lblRunningAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAddToAvg
+            // 
+            this.btnAddToAvg.Location = new System.Drawing.Point(172, 74);
+            this.btnAddToAvg.Name = "btnAddToAvg";
+            this.btnAddToAvg.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToAvg.TabIndex = 0;
+            this.btnAddToAvg.Text = "Add";
+            this.btnAddToAvg.UseVisualStyleBackColor = true;
+            this.btnAddToAvg.Click += new System.EventHandler(this.btnAddToAvg_Click);
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(46, 76);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 1;
+            // 
+            // lblAddToAvg
+            // 
+            this.lblAddToAvg.AutoSize = true;
+            this.lblAddToAvg.Location = new System.Drawing.Point(43, 40);
+            this.lblAddToAvg.Name = "lblAddToAvg";
+            this.lblAddToAvg.Size = new System.Drawing.Size(210, 13);
+            this.lblAddToAvg.TabIndex = 2;
+            this.lblAddToAvg.Text = "Enter a mark to add to the running average";
+            // 
+            // lblRunningAverage
+            // 
+            this.lblRunningAverage.AutoSize = true;
+            this.lblRunningAverage.Location = new System.Drawing.Point(43, 165);
+            this.lblRunningAverage.Name = "lblRunningAverage";
+            this.lblRunningAverage.Size = new System.Drawing.Size(116, 13);
+            this.lblRunningAverage.TabIndex = 3;
+            this.lblRunningAverage.Text = "The running average is";
+            // 
+            // frmRunningAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblRunningAverage);
+            this.Controls.Add(this.lblAddToAvg);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.btnAddToAvg);
+            this.Name = "frmRunningAverage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddToAvg;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Label lblAddToAvg;
+        private System.Windows.Forms.Label lblRunningAverage;
+    }
+}
+
---
+++ RunningAverageBenF/RunningAverageBenF/RunningAverageForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningAverageBenF
+{
+    public partial class frmRunningAverage : Form
+    {
+        double numMarks = 0, average = 0, total = 0;
+
+        public frmRunningAverage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddToAvg_Click(object sender, EventArgs e)
+        {
+            double mark = 0;
+
+            mark = double.Parse(txtMark.Text);
+            if (mark > 0 && mark < 100)
+            {
+                total = total + mark;
+                numMarks++;
+                average = total / numMarks;
+                lblRunningAverage.Text = ("Running average = " + average);
+            }
+            else if (mark == -1)
+            {
+                MessageBox.Show("The final average is " + average + "%");
+                txtMark.Enabled = false;
+                btnAddToAvg.Enabled = false;
+                lblRunningAverage.Text = ("The final average is " + average + "%");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a mark between 1 and 100 or enter -1 to end the program");
+            }
+        }
+    }
+}
