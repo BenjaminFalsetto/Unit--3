32c805106aab5ae12520b3558ee811882ea5514f
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-22 10:26:45 -0500

    update

---
+++ PerfectSquaresBenF/PerfectSquaresBenF/PerfectSquaresForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PerfectSquaresBenF
+{
+    partial class PerfectSquaresForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudEndVal = new System.Windows.Forms.NumericUpDown();
+            this.lstSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndVal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(105, 60);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudEndVal
+            // 
+            this.nudEndVal.Location = new System.Drawing.Point(82, 109);
+            this.nudEndVal.Name = "nudEndVal";
+            this.nudEndVal.Size = new System.Drawing.Size(120, 20);
+            this.nudEndVal.TabIndex = 1;
+            // 
+            // lstSquares
+            // 
+            this.lstSquares.FormattingEnabled = true;
+            this.lstSquares.Location = new System.Drawing.Point(82, 154);
+            this.lstSquares.Name = "lstSquares";
+            this.lstSquares.Size = new System.Drawing.Size(120, 95);
+            this.lstSquares.TabIndex = 2;
+            // 
+            // PerfectSquaresForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstSquares);
+            this.Controls.Add(this.nudEndVal);
+            this.Controls.Add(this.btnStart);
+            this.Name = "PerfectSquaresForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndVal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudEndVal;
+        private System.Windows.Forms.ListBox lstSquares;
+    }
+}
+
---
+++ PerfectSquaresBenF/PerfectSquaresBenF/PerfectSquaresForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquaresBenF
+{
+    public partial class PerfectSquaresForm : Form
+    {
+        public PerfectSquaresForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //deckare local variables               
+            int endingValue;
+            int value;
+            Double rootAsDouble;
+            int rootAsInt;
+
+
+        }
+    }
+}
