81f63bad2b200ca6c5e7a56c5188608ee7a624d5
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-12-01 14:33:40 -0500

    I think I'm done now

---
+++ FactorialForLoopBenF/FactorialForLoopBenF/FactorialForLoopForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FactorialForLoopBenF
+{
+    partial class frmFactorialForLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.txtUserVal = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(85, 153);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstNumbers.TabIndex = 0;
+            // 
+            // txtUserVal
+            // 
+            this.txtUserVal.Location = new System.Drawing.Point(95, 107);
+            this.txtUserVal.Name = "txtUserVal";
+            this.txtUserVal.Size = new System.Drawing.Size(100, 20);
+            this.txtUserVal.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(108, 62);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmFactorialForLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 297);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtUserVal);
+            this.Controls.Add(this.lstNumbers);
+            this.Name = "frmFactorialForLoop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factorial with For Loop Benjamin Falsetto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.TextBox txtUserVal;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ FactorialForLoopBenF/FactorialForLoopBenF/FactorialForLoopForm.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by: Benjamin Falsetto
+ * Created on: 2018/11/10
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #23 - Factorial with for loop
+ * This program takes a user value and calculates the factorial of that
+ * number and displays all the numbers up until that number
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialForLoopBenF
+{
+    public partial class frmFactorialForLoop : Form
+    {
+        public frmFactorialForLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            double userVal, factorialAns = 1;
+            int counter;
+
+            //clear the listbox
+            this.lstNumbers.Items.Clear();
+
+            //get the user value
+            userVal = Convert.ToDouble(txtUserVal.Text);
+
+            //loop through all the numbers up until the userVal
+            for (counter = 1; counter <= userVal; counter++)
+            {
+                //add the current value to the listbox
+                lstNumbers.Items.Add(counter);
+
+                //calculate the answer
+                factorialAns = factorialAns * counter;
+            }
+
+            //display the answer to the user
+            MessageBox.Show(userVal + "! = " + factorialAns);
+        }
+    }
+}
